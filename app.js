166c128985f2cf4cf7a02af583ede91a4129bfc8
Rafael <76389145+rafakk@users.noreply.github.com>
2023-08-08 13:19:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hello world")
+// Função para realizar a soma de dois números
+function realizarSoma(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+// Exemplo de uso da função
+var primeiroNumero = 5;
+var segundoNumero = 10;
+
+var resultado = realizarSoma(primeiroNumero, segundoNumero);
+
+console.log("Resultado:", resultado);
+
